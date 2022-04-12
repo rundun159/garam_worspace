d005e90a674f1f908ffdd42c8e502f5f20e53fda
TH <xogud1231@gmail.com>
2022-04-12 18:22:48 +0900

    modify adding logo

--- overlay_logo.py
+++ overlay_logo.py
@@ -1,9 +1,13 @@
 import cv2
 import os
 import json
+import argparse
 
 DEBUG_PAR = True
 
+parser = argparse.ArgumentParser(description='overlay_logo')
+parser.add_argument('--src_name', type=str)
+args = parser.parse_args()
 
 def overlay_img(background, overlay, start_point, dim):
     if DEBUG_PAR: print(f"overlay shape : {overlay.shape}, dim : {dim}")
@@ -16,6 +20,7 @@ def overlay_img(background, overlay, start_point, dim):
             background_color = background[start_point[0] + y, start_point[1] + x]
 
             # combine the background color and the overlay color weighted by alpha
+            overlay_alpha *= 0.5
             composite_color = background_color * (1 - overlay_alpha) + overlay_color * overlay_alpha
 
             # update the background image in place
@@ -35,22 +40,23 @@ def ret_start_point(h, w, padding_rate, dim, flag):
         start_point = (h - padding_size - dim[0],padding_size)
     elif (flag == '4'):
         start_point = (h - padding_size - dim[0], w - padding_size - dim[1])
-
+    if DEBUG_PAR : print(f"start_point : {start_point}")
     return start_point
 
 if __name__ == '__main__':
-    dir_name = '..\\imgs\\data_annotated'
+    print("It changed!")
+    dir_name = f'..\\imgs\\{args.src_name}'
     new_dir_name = dir_name + '_logo'
     os.makedirs(new_dir_name, exist_ok=True)
     files = os.listdir(dir_name)
     n_json_list = [name for name in files if name.split('.')[-1] !='json']
     file_name_list = ['.'.join(file.split('.')[:-1]) for file in n_json_list]
 
-    logo_path = '..\\imgs\\logo\\TLC-favicon-new.png'
+    logo_path = '..\\imgs\\logo\\TLC-favicon-high-alpha-high-resolution.png'
     logo = cv2.imread(logo_path, cv2.IMREAD_UNCHANGED)
     logo_h, logo_w = logo.shape[:2]
     if DEBUG_PAR : print("Logo Shape : ", logo.shape)
-    padding_rate = 0.02
+    padding_rate = 0
     resize_rate = 0.1
     cnt = 0 
 
@@ -71,10 +77,10 @@ if __name__ == '__main__':
 
                 if DEBUG_PAR: print(f"Img Shape : {background.shape}, {file_name}")
 
-                # dim = (int(resize_rate * h / logo_h), int(resize_rate * w / logo_w))
+                garam_w = 100
+                dim = (garam_w, int(garam_w * logo_w / logo_h))
                 resize_rate_min = min(resize_rate * h / logo_h, resize_rate * w / logo_w)
-                dim = (int(logo_h * resize_rate_min), int(logo_w * resize_rate_min))
-                
+
                 if DEBUG_PAR: print(f"dim : {dim}")
 
                 start_point = ret_start_point(h,w,padding_rate,dim,flag)
